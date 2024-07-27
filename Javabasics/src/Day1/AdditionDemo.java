f6c820972bf85966f67fcfe1a5103cb474826005
keerthiga <keerthiga@LAPTOP-0O03JT2V>
2024-07-27 09:58:39 +0530

    i completed day one project

---
+++ Javabasics/src/Day1/AdditionDemo.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=5;
+		b=4;
+		c= a+b;
+		System.out.println("addition:"+c);
+			
+
+	}
+
+}
